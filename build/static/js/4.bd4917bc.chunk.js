4c44d721a5abc1ad01ffe0c80e7bae63f44210b8
秦小波 <china16qin@gmail.com>
2019-10-22 15:53:53 +0800

    remove buidl

--- build/static/js/4.bd4917bc.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonptest=this.webpackJsonptest||[]).push([[4],{173:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return l}));var c=n(40),r=n(44),a=n(42),u=n(41),s=n(43),i=n(0),l=function(t){function e(t){var n;return Object(c.a)(this,e),(n=Object(a.a)(this,Object(u.a)(e).call(this,t))).state={},n}return Object(s.a)(e,t),Object(r.a)(e,[{key:"render",value:function(){return i.createElement("div",null,i.createElement("h1",null,"\u7b2c\u4e00\u5f20"))}}]),e}(i.Component)}}]);
-//# sourceMappingURL=4.bd4917bc.chunk.js.map
--- build/static/js/5.fa742979.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonptest=this.webpackJsonptest||[]).push([[5],{174:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return l}));var c=n(40),r=n(44),a=n(42),u=n(41),s=n(43),i=n(0),l=function(t){function e(t){var n;return Object(c.a)(this,e),(n=Object(a.a)(this,Object(u.a)(e).call(this,t))).state={},n}return Object(s.a)(e,t),Object(r.a)(e,[{key:"render",value:function(){return i.createElement("div",null,i.createElement("h1",null,"\u7b2c\u4e8c\u5f20"))}}]),e}(i.Component)}}]);
-//# sourceMappingURL=5.fa742979.chunk.js.map
