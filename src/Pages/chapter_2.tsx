a7f65fa51bc7f9a6cd0fec31487479e50af0f594
秦小波 <china16qin@gmail.com>
2019-10-26 18:08:50 +0800

    earth rotate

--- src/Pages/chapter_2.tsx
+++ src/Pages/chapter_2.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Scene, WebGLRenderer, PerspectiveCamera, SphereGeometry, MeshPhongMaterial, Mesh, Color, DirectionalLight, Texture, DoubleSide, BackSide, MeshBasicMaterial, TextureLoader } from 'three';
+import { Scene, WebGLRenderer, PerspectiveCamera, SphereGeometry, MeshPhongMaterial, Mesh, Color, DirectionalLight, Texture, DoubleSide, BackSide, MeshBasicMaterial, TextureLoader, MeshLambertMaterial, Vector3 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 export interface IChapter1Props {
 }
@@ -17,6 +17,8 @@ export default class Chapter2 extends React.Component<IChapter1Props, IChapter1S
   private containerRef = React.createRef<HTMLDivElement>()
   componentDidMount() {
     if (this.containerRef.current) {
+      const camOffset = 100;
+      let direction = new Vector3();
       let width = this.containerRef.current.clientWidth;
       let height = this.containerRef.current.clientHeight;
       let scene = new Scene();
@@ -25,20 +27,44 @@ export default class Chapter2 extends React.Component<IChapter1Props, IChapter1S
       let camera = this.createCamera(width, height);
       let controls = new OrbitControls(camera, renderer.domElement);
       controls.autoRotate = true;
-      controls.maxDistance = 800;
+      controls.maxDistance = 1500;
+      // controls.enablePan = false;
+      // controls.enableZoom = false;
       let earth = this.createEarth()
+      let earthSmall = this.createEarth()
+      earthSmall.position.set(0, 0, 450);
       scene.add(earth);
+      scene.add(earthSmall);
       scene.add(this.createDirectLight());
       scene.add(this.createBackground())
+      const Sun = new Mesh(new SphereGeometry(10.2, 16, 16),
+        new MeshLambertMaterial({
+          color: 0xffff00,
+          emissive: 0xdd4422,
+          opacity: 0
+        })
+      );
+      Sun.name = 'Sun';
+      scene.add(Sun);
       this.makeCanvasCloud().then(f => {
         let cloud = this.createEarthCloud(f);
         scene.add(cloud)
         if (this.containerRef.current) {
           this.containerRef.current.appendChild(renderer.domElement);
+          let angle = 0;
           let animate = () => {
+            requestAnimationFrame(animate);
+            // direction.subVectors(camera.position, controls.target);
+            // direction.normalize().multiplyScalar(camOffset);
+            // camera.position.copy(direction.add(controls.target));
+            // earth.getWorldPosition(controls.target)
+            controls.target = new Vector3().copy(earth.position)
+            angle += 0.01;
             earth.rotation.y += 0.001;
             cloud.rotation.y += 0.002;
-            requestAnimationFrame(animate);
+            earthSmall.position.set(450 * Math.sin(angle), 0, 450 * Math.cos(angle));
+            // cloud.position.set(500 * Math.sin(angle), 0, 500 * Math.cos(angle));
+            controls.update();
             renderer.render(scene, camera);
           }
           animate()
@@ -103,29 +129,29 @@ export default class Chapter2 extends React.Component<IChapter1Props, IChapter1S
 
   // 创建camera
   createCamera(width: number, height: number) {
-    let camera = new PerspectiveCamera(45, width / height, 0.1, 1600);
-    camera.position.set(-80, 200, 500);
-    camera.lookAt(0, 0, 0);
+    let camera = new PerspectiveCamera(45, width / height, 0.1, 10000);
+    camera.position.set(0, 200, 0);
+    // camera.lookAt(0, 0, 0);
     return camera
   }
 
   // 创建地球
   createEarth() {
-    let geomary = new SphereGeometry(80, 80, 80);
+    let geomary = new SphereGeometry(8, 32, 32);
     let material = new MeshPhongMaterial({
-      map: new TextureLoader().load('./images/earthmap1k.jpg'),
-      bumpMap: new TextureLoader().load('./images/earthbump1k.jpg'),
-      bumpScale: 1,
-      specularMap: new TextureLoader().load('./images/earthspec1k.jpg'),
-      specular: new Color('#666'),
+      map: new TextureLoader().load('./images/earth1.jpg'),
+      bumpMap: new TextureLoader().load('./images/bathymetry.jpg'),
+      bumpScale: 1.5,
       shininess: 5
     })
-    return new Mesh(geomary, material);
+    let Earth = new Mesh(geomary, material);
+    Earth.position.set(0, 0, 500)
+    return Earth;
   }
   // 创建地球云层
 
   createEarthCloud(cloud: HTMLCanvasElement) {
-    let geomary = new SphereGeometry(80.2, 90, 90);
+    let geomary = new SphereGeometry(8.2, 32, 32);
     let cloudTexture = new Texture(cloud);
     cloudTexture.needsUpdate = true;
     let material = new MeshPhongMaterial({
@@ -135,11 +161,12 @@ export default class Chapter2 extends React.Component<IChapter1Props, IChapter1S
       transparent: true
     })
     let mesh = new Mesh(geomary, material);
+    mesh.position.set(0, 0, 500)
     return mesh;
   }
   // 创建背景图
   createBackground() {
-    let geomery = new SphereGeometry(800, 32, 32);
+    let geomery = new SphereGeometry(1500, 32, 32);
     let material = new MeshBasicMaterial({
       map: new TextureLoader().load('./images/galaxy_starfield.png'),
       side: BackSide
@@ -150,7 +177,7 @@ export default class Chapter2 extends React.Component<IChapter1Props, IChapter1S
   // 创建点光源
   createDirectLight() {
     let light = new DirectionalLight(0xffffff, 1.5);
-    light.position.set(300, 200, 500);
+    light.position.set(10, 0, 0);
     return light;
   }
   public render() {
